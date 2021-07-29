f8bbc058d82babe2a50da7dacb9fb68cff4c77c9
jaennpb <jaenn.bucio@gmail.com>
2021-07-29 16:51:20 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const [enteredText, setEnteredText] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const changeEnteredTextHandler = (text) => {
+    setEnteredText(text);
+  };
+
+  const setGoalsHandler = () => {
+    setGoals((oldState) => [
+      ...oldState,
+      { key: Math.random().toString(), name: enteredText },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screenContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Goal"
+          style={styles.input}
+          onChangeText={changeEnteredTextHandler}
+          value={enteredText}
+        />
+        <Button title="ADD" onPress={setGoalsHandler} />
+      </View>
+      <FlatList
+        data={goals}
+        renderItem={(itemData) => (
+          <View style={styles.goalContainer}>
+            <Text>{itemData.item.name}</Text>
+          </View>
+        )}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screenContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  input: {
+    width: "80%",
+    borderBottomColor: "grey",
+    borderBottomWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+  },
+  goalContainer: {
+    padding: 10,
+    borderLeftWidth: 1,
+    borderColor: "blue",
+    marginBottom: 10,
   },
 });
+
+export default App;
